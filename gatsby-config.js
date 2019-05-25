11e939181f594fcfd2c62594fbcf4e1f30022fcf
Terje Uglebakken <teug91@gmail.com>
2019-05-25 14:40:12 +0200

    chore: d2-style js install

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,23 @@ module.exports = {
         'gatsby-plugin-styled-jsx',
         'gatsby-plugin-styled-components',
         {
-            resolve: `gatsby-transformer-remark`,
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: 'DHIS2 Design Lab',
+                short_name: 'DHIS2 Design Lab',
+                start_url: '/',
+                background_color: '#ebebeb',
+                theme_color: '#215e8c',
+                display: 'minimal-ui',
+                icon: 'src/images/dhis2.png',
+            },
+        },
+        {
+            resolve: 'gatsby-transformer-remark',
             options: {
                 plugins: [
                     {
-                        resolve: `gatsby-remark-vscode`,
+                        resolve: 'gatsby-remark-vscode',
                     },
                 ],
             },
